4b8c71a74b655bc8d4360d6ca9c58c7aa8ee4110
Tanuj Vishnoi <tanujvishnoi1306@gmail.com>
2022-06-30 12:27:14 +0530

    working with lite mode

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static('app'));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 app.listen(port, () => console.log(`background remover listening on port ${port}!`));
